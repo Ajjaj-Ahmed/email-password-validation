8ed9d4cec9ec2b847789e3eaef36bea4c50de35b
ajjaj ahmed <ajjajahmedasif@gmail.com>
2024-11-13 14:04:26 +0600

    adding route and firebase

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,104 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from '../../firebase/firebase.init';
+import { FaRegEye, FaEyeSlash } from "react-icons/fa";
+
+const Register = () => {
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [success, setSuccess] = useState(false);
+    const [eye, setEye] = useState(false)
+
+    const handleEyeButton = (value) => {
+        setEye(value);
+    }
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const terms = event.target.terms.checked;
+        console.log(email, password, terms)
+
+        // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/;
+
+
+        setErrorMessage('');
+        setSuccess(false)
+
+
+        // if (password.length < 6) {
+        //     setErrorMessage('Password should be 6 digits');
+        //     return
+        // }
+        // if (!passwordRegex.test(password)) {
+        //     setErrorMessage('Password should have one upper ,one lower and one speical character')
+        //     return
+        // }
+
+        if(!terms){
+            setErrorMessage('Check the conditon');
+            return
+        }
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => setSuccess(true))
+            .catch(error => setErrorMessage(error.message))
+
+
+    }
+
+    return (
+        <div className='max-w-lg mx-auto text-center'>
+            <h2 className='text-4xl my-8'>Register</h2>
+            <form onSubmit={handleRegister}>
+                <label className="input input-bordered flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                        <path
+                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                    </svg>
+                    <input type="email" name='email' className="grow" placeholder="Email" />
+                </label>
+
+                <label className="input input-bordered flex items-center gap-2 my-8">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                            fillRule="evenodd"
+                            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                            clipRule="evenodd" />
+                    </svg>
+                    <input type={eye ? 'text' : 'password'} placeholder='Password' name='password' className="grow" />
+                    <button onClick={() => handleEyeButton(!eye)}>
+                        {eye ? <FaEyeSlash /> : <FaRegEye />}
+                    </button>
+                </label>
+
+                {/* check box */}
+                <div className='text-center my-4'>
+                    <label className="cursor-pointer justify-start gap-2 label">
+                        <input type="checkbox" name='terms' className="checkbox checkbox-secondary" />
+                        <span className="label-text">Accept the terms and condition</span>
+                    </label>
+                </div>
+
+                <button className='btn btn-primary btn-wide'>Submit</button>
+            </form>
+            {
+                errorMessage && <p className='text-red-400 pt-3'>{errorMessage}</p>
+            }
+            {
+                success && <p className='text-green-500 pt-3'>Sign in successfully</p>
+            }
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Register from './components/Register/Register.jsx';
+import Login from './components/Login/Login.jsx';
+import SignUp from './components/SignUp/SignUp.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/register',
+        element:<Register></Register>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+      {
+        path:'/signup',
+        element:<SignUp></SignUp>
+      }
+  ]},
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
